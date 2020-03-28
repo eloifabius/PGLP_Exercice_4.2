b1aec67a74a9e3d7336a8bc79406c8b082043fb8
eloifabius <fabius1995>
2020-03-28 21:35:19 +0100

    creation de l'interface Command

---
+++ src/main/java/fr/uvsq/pglp/Command.java
@@ -0,0 +1,5 @@
+package fr.uvsq.pglp;
+
+public interface Command {
+    public void apply();
+}
--- src/main/java/fr/uvsq/pglp/Interpreteur.java
+++
@@ -1,6 +0,0 @@
-package fr.uvsq.pglp;
-
-public interface Interpreteur {
-    public void quit();
-    public void undo();
-}
